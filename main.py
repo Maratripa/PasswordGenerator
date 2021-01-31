7e4412d3144c7741092931a29b066114c996b904
Martin <maratripa@gmail.com>
2021-01-31 20:21:11 -0300

    More options

--- main.py
+++ main.py
@@ -8,30 +8,39 @@ with open('Words.txt', 'r') as file:
     word_list.append(i[:-1])
 
 
-def generate_password(foo):
+def generate_password(foo, separator, words):
 
-  random_words = [random.choice(word_list), random.choice(word_list), random.choice(word_list)]
+  random_words = []
+  for i in range(words):
+    random_words.append(random.choice(word_list))
 
   if foo:
     for i in random_words:
       random_words[random_words.index(i)] = random_words[random_words.index(i)].title()
   
-  password = '{digits}{punctuation}{word1}{punctuation}{word2}{punctuation}{word3}'.format(digits=random.randint(10,100), punctuation='-', word1=random_words[0], word2=random_words[1], word3=random_words[2])
+  password = '{digits}'.format(digits=random.randint(10,100))
+
+  for i in random_words:
+    password = password + '{punctuation}{word0}'.format(punctuation=separator, word0=random_words[random_words.index(i)])
 
   return password
+
+
+def str2bool(v):
+  return v.lower() in ("true", "1")
 
 
 def run():
   raw = input('Enter your command: ')
   raw_list = raw.split()
 
-  options = ['random', 0]
+  options = ['random', False, '-', 3]
 
   for i in raw_list:
     options[raw_list.index(i)] = i
 
   if options[0] == 'random':
-    password = generate_password(options[1])
+    password = generate_password(str2bool(options[1]), options[2], int(options[3]))
     print(password)
 
 run()
